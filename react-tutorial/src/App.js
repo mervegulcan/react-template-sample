ed17e867e3617ab0b641a89fc2ea49a7490b8065
mervegulcan <gulcan.mrv@gmail.com>
2021-09-14 08:58:23 +0300

    react-template

---
+++ react-tutorial/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Header from './components/Header';
+import Navbar from './components/navbar';
+Navbar
+
+
+
+
+
+function App() {  
+
+  return (
+    <> 
+    <Navbar></Navbar>
+    <Header></Header>
+    </>
+  );
+}
+
+export default App;
